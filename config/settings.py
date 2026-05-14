c864cdaa9ca85e761167e32c2f66ec56fd2ac1a4
Vandhana Mulguri <vandhanamulguri@gmail.com>
2026-05-14 11:07:50 +0530

    deployment setup

--- config/settings.py
+++ config/settings.py
@@ -26,9 +26,8 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = os.getenv("DEBUG", "False") == "True"
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
