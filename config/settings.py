5226f9cf2f343729c7a376ca6cd73c2098de70dd
Vandhana Mulguri <vandhanamulguri@gmail.com>
2026-05-14 11:11:21 +0530

    Fixed static root

--- config/settings.py
+++ config/settings.py
@@ -136,10 +136,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 
 CELERY_ACCEPT_CONTENT = ['json']
 
 CELERY_TASK_SERIALIZER = 'json'
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
